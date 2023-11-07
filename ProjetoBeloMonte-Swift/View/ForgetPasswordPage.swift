03e1c591b177c7312d40a3c87ba7f99ab3a3ed0c
Theo Furtado <58632127+theofurtado05@users.noreply.github.com>
2023-11-07 17:20:53 -0300

    forgotpass e register

--- ProjetoBeloMonte-Swift/View/ForgetPasswordPage.swift
+++ ProjetoBeloMonte-Swift/View/ForgetPasswordPage.swift
@@ -8,9 +8,84 @@
 import SwiftUI
 
 struct ForgetPasswordPage: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @State var email: String = ""
+    @State var password: String = ""
+    
+    @State var emailCerto: String = "Admin@admin"
+    @State var passwordCerta: String = "123"
+    
+    @State var isAuthenticated: Bool = false
+    
+    func checkLogin() {
+        if email == emailCerto && password == passwordCerta{
+            isAuthenticated = true
+            print(isAuthenticated)
+            
+            
+        } else {
+            isAuthenticated = false
+        }
     }
+    
+    
+    
+    var body: some View {
+        NavigationView{
+            
+            ZStack{
+                Color("BackgroundColor").ignoresSafeArea()
+                
+                VStack{
+                    Image("Logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200.0)
+                    
+                    Spacer().frame(height: 80)
+                    
+                    VStack{
+                        TextField("Email", text: $email)
+                            .padding(10)
+                            .background(Color("InputBackground"))
+                            .cornerRadius(8.0)
+                        
+                        Spacer().frame(height: 10)
+
+                    }
+                    
+                    
+                    Spacer().frame(height: 20)
+                
+                    
+                    NavigationLink(destination: MenuView()) {
+                        Text("Recuperar senha")
+                            .font(.title2)
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding(10)
+                            .frame(maxWidth: .infinity)
+                            .background(Color("ButtonColor"))
+                            .cornerRadius(8)
+                    }
+                    
+                    
+                    NavigationLink(destination: ContentView()) {
+                        Text("Voltar")
+                            .font(.title3)
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding(0)
+                            .frame(maxWidth: .infinity)
+                    }
+                    
+                    
+                }.padding()
+            }
+            
+        }
+        
+    }
+    
 }
 
 struct ForgetPasswordPage_Previews: PreviewProvider {
--- ProjetoBeloMonte-Swift/View/RegisterPage.swift
+++ ProjetoBeloMonte-Swift/View/RegisterPage.swift
@@ -8,9 +8,123 @@
 import SwiftUI
 
 struct RegisterPage: View {
-    var body: some View {
-        Text("Pagina de registro")
+    @State var email: String = ""
+    @State var password: String = ""
+    
+    @State var emailCerto: String = "Admin@admin"
+    @State var passwordCerta: String = "123"
+    
+    @State var isAuthenticated: Bool = false
+    
+    func checkLogin() {
+        if email == emailCerto && password == passwordCerta{
+            isAuthenticated = true
+            print(isAuthenticated)
+            
+            
+        } else {
+            isAuthenticated = false
+        }
     }
+    
+    
+    
+    var body: some View {
+        NavigationView{
+            
+            ZStack{
+                Color("BackgroundColor").ignoresSafeArea()
+                
+                VStack{
+                    Image("Logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200.0)
+                    
+                    Spacer().frame(height: 80)
+                    
+                    VStack{
+                        TextField("Email", text: $email)
+                            .padding(10)
+                            .background(Color("InputBackground"))
+                            .cornerRadius(8.0)
+                        
+                        Spacer().frame(height: 10)
+                        
+                        TextField("Nome", text: $email)
+                            .padding(10)
+                            .background(Color("InputBackground"))
+                            .cornerRadius(8.0)
+                        
+                        Spacer().frame(height: 10)
+                        
+                        TextField("Cargo", text: $email)
+                            .padding(10)
+                            .background(Color("InputBackground"))
+                            .cornerRadius(8.0)
+                        
+                        Spacer().frame(height: 10)
+                        
+                        SecureField("Senha", text: $password)
+                            .padding(10)
+                            .background(Color("InputBackground"))
+                            .cornerRadius(8.0)
+                        
+                        Spacer().frame(height: 10)
+                        
+                        SecureField("Confirmar Senha", text: $password)
+                            .padding(10)
+                            .background(Color("InputBackground"))
+                            .cornerRadius(8.0)
+                    }
+                    
+                    
+                    Spacer().frame(height: 20)
+                
+                    
+                    NavigationLink(destination: MenuView()) {
+                        Text("Entrar")
+                            .font(.title2)
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding(10)
+                            .frame(maxWidth: .infinity)
+                            .background(Color("ButtonColor"))
+                            .cornerRadius(8)
+                    }
+                    .alert(isPresented: $isAuthenticated) {
+                        Alert(
+                            title: Text("Autenticação"),
+                            message: Text("Login bem sucedido!"),
+                            dismissButton: .default(Text("OK"))
+                        )
+                    }
+                    
+                    NavigationLink(destination: ForgetPasswordPage()) {
+                        Text("Esqueceu sua senha?")
+                            .font(.title3)
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding(0)
+                            .frame(maxWidth: .infinity)
+                    }
+                    
+                    NavigationLink(destination: RegisterPage()) {
+                        Text("Registre-se")
+                            .font(.title3)
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding(0)
+                            .frame(maxWidth: .infinity)
+                    }
+                    
+                }.padding()
+            }
+            
+        }
+        
+    }
+    
 }
 
 struct RegisterPage_Previews: PreviewProvider {
