af64026471fea23965152afd9fb8216bb5ab920d
Theo Furtado <58632127+theofurtado05@users.noreply.github.com>
2023-11-26 21:24:26 -0300

    .

--- ProjetoBeloMonte-Swift/View/ActiveReuniao.swift
+++ ProjetoBeloMonte-Swift/View/ActiveReuniao.swift
@@ -11,15 +11,26 @@ struct ActiveReuniao: View {
     
     
     var body: some View {
-        Text(Sistema.shared.activeReuniao.nomeEvento)
-        Text(Sistema.shared.activeReuniao.descricao)
-        Text(Sistema.shared.activeReuniao.localizacao)
-        Text(Sistema.shared.activeReuniao.notificacao)
-        
-        
+        ZStack{
+            VStack{
+                HeaderTitleView(header: HeaderTitle(title: "Titulo da Reuniao", goTo: AnyView(MenuView())))
+                VStack(alignment: .leading, spacing: 10) {
+                    HStack{
+                        Image(systemName: "calendar")
+                        VStack(alignment: .leading, spacing: 0){
+                            Text("Data")
+                            Text("26/11/2023")
+                        }
+                    }.frame(width: .infinity)
+                    
+                }
+                .frame(width: .infinity)
+                
+            }
+        }
     }
 }
 
 #Preview {
-    MenuView()
+    ActiveReuniao()
 }
